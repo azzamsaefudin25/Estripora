272241e1119b74902317fa7f8b315d0d8d82f4f6
Azzam Saefudin <azzamsaefudin.asr@gmail.com>
2025-02-07 18:35:05 +0700

    Penyewaan

--- app/Filament/Resources/UlasanResource.php
+++ app/Filament/Resources/UlasanResource.php
@@ -2,22 +2,63 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\UlasanResource\Pages;
-use App\Filament\Resources\UlasanResource\RelationManagers;
-use App\Models\Ulasan;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use App\Models\Ulasan;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\UlasanResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\UlasanResource\RelationManagers;
 
 class UlasanResource extends Resource
 {
     protected static ?string $model = Ulasan::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function getNavigationLabel(): string
+    {
+        return 'Reviews'; // Ganti dengan nama yang kamu inginkan
+    }
+    public static function getPluralLabel(): string
+    {
+        return 'Reviews'; // Ganti dengan nama yang sesuai
+    }
+    public static function getModelLabel(): string
+    {
+        return 'Review';
+    }
+
+    public static function canAccess(): bool
+    {
+        return Auth::check() && Auth::user()->role === 'admin';
+    }
+
+    public static function canCreate(): bool
+    {
+        return Auth::check() && Auth::user()->role === 'admin';
+    }
+
+    public static function canEdit(Model $record): bool
+    {
+        return Auth::check() && Auth::user()->role === 'admin';
+    }
+
+    public static function canDelete(Model $record): bool
+    {
+        return Auth::check() && Auth::user()->role === 'admin';
+    }
+
+    public static function canView(Model $record): bool
+    {
+        return Auth::check() && Auth::user()->role === 'admin';
+    }
+
 
     public static function form(Form $form): Form
     {
