b236dfcf36ee33810b9dda282306255be6bf1edf
Azzam Saefudin <azzamsaefudin.asr@gmail.com>
2025-06-01 22:03:57 +0700

    update tampilan

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\PenyewaanResource;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\Widgets;
@@ -41,6 +42,7 @@ class AdminPanelProvider extends PanelProvider
             ->widgets([
                 // Widgets\AccountWidget::class,
                 // Widgets\FilamentInfoWidget::class,
+                PenyewaanResource\Widgets\StatistikPenyewaan::class,
             ])
             ->middleware([
                 EncryptCookies::class,
